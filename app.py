7c3cc54e8b342a546114b4a873c6413d7720fa14
Kiyoshi-gg <topgansta228@mail.ru>
2026-09-22 18:30:15 +0800

    Подключен SQLAlchemy, создан extensions.py для устранения циклического импорта, создана БД с моделями User,Ingredient,Recipe

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from extensions import db
+from models import User ,Recipe, Ingredient
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from extensions import db
+
+recipe_ingredient = db.Table('recipe_ingredient', db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
+                             db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id'), primary_key=True),
+                             db.Column('quantity', db.String(50)))
+favorites = db.Table('favorites', db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
+                     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True))
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    recipes = db.relationship('Recipe', backref='author', lazy=True, cascade='all, delete-orphan')
+    favorite_recipes = db.relationship('Recipe', secondary=favorites, backref=db.backref('favorited_by', lazy='dynamic'), lazy='dynamic')
+
+    def set_password(self, password):
+       self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+       return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Ingredient(db.Model):
+    __tablename__ = 'ingredient'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    def __repr__(self):
+        return f'<Ingredient {self.name}>'
+
+class Recipe(db.Model):
+    __tablename__ = 'recipe'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    steps = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    ingredients = db.relationship('Ingredient', secondary=recipe_ingredient, backref=db.backref('recipes', lazy='dynamic'), lazy='joined')
+    def __repr__(self):
+        return f'<Recipe {self.title}>'
